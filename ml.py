a40aa5dd8443b70be245c45a0c4bad188fd5b29e
prerana mandalika <43927565+Sprerana@users.noreply.github.com>
2020-06-25 14:00:42 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -19,7 +19,5 @@ m = MarkovClickstream(data)
 fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
 sns.heatmap(m.prob_matrix, xticklabels=m.pages, yticklabels=m.pages,cmap="YlGnBu")
 from markovclick.viz import visualise_markov_chain
-    graph = visualise_markov_chain(m)
+graph = visualise_markov_chain(m)
 print(graph.source)
-graph.render('test-output/round-table.gv', view=True)
-graph
